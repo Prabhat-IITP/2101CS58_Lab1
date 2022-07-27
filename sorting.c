4639f73401f3df93210f488088eaee73cb7fbcd0
Prabhat <prabhat_2101cs58@iitp.ac.in>
2022-07-27 23:28:43 +0530

    Bubble sort

--- sorting.c
+++ sorting.c
@@ -32,3 +32,12 @@ void selectionSort(int arr[], int n)
         swap(&arr[min_index], &arr[i]); 
     } 
 } 
+
+void bubbleSort(int arr[], int n) 
+{ 
+    int i, j; 
+    for (i = 0; i < n - 1; i++) 
+        for (j = 0; j < n - i - 1; j++) 
+            if (arr[j] > arr[j + 1]) 
+                swap(&arr[j], &arr[j + 1]); 
+} 
