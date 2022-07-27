c14d4cc4ad43f9b5284f8d1a1af6e216df2ee2ea
Prabhat <prabhat_2101cs58@iitp.ac.in>
2022-07-27 23:40:41 +0530

    Quick sort

--- sorting.c
+++ sorting.c
@@ -86,3 +86,27 @@ void mergeSort(int arr[], int l, int r)
     } 
 } 
 
+void quickSort(int arr[],int first,int last){ 
+   int i, j, pivot, temp; 
+   if(first<last){ 
+      pivot=first; 
+      i=first; 
+      j=last; 
+      while(i<j){ 
+         while(arr[i]<=arr[pivot]&&i<last) 
+         i++; 
+         while(arr[j]>arr[pivot]) 
+         j--; 
+         if(i<j){ 
+            temp=arr[i]; 
+            arr[i]=arr[j]; 
+            arr[j]=temp; 
+         } 
+      } 
+      temp=arr[pivot]; 
+      arr[pivot]=arr[j]; 
+      arr[j]=temp; 
+      quickSort(arr,first,j-1); 
+      quickSort(arr,j+1,last); 
+   } 
+} 
