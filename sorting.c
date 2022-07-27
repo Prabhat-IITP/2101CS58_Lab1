d255e2f9f71da116ee14d71c96674fb9bef99724
Prabhat <prabhat_2101cs58@iitp.ac.in>
2022-07-27 23:24:21 +0530

    Selection sort

--- sorting.c
+++ sorting.c
@@ -20,3 +20,15 @@ void insertionSort(int arr[], int n)
         arr[j + 1] = key; 
     } 
 } 
+
+void selectionSort(int arr[], int n) 
+{ 
+    int i, j, min_index; 
+    for (i = 0; i < n-1; i++) { 
+        min_index = i; 
+        for (j = i+1; j < n; j++) 
+          if (arr[j] < arr[min_index]) 
+            min_index = j; 
+        swap(&arr[min_index], &arr[i]); 
+    } 
+} 
