7d79310a3f660fc28953bbf27d178cf9092bbae2
Prabhat <prabhat_2101cs58@iitp.ac.in>
2022-07-27 23:20:50 +0530

    Insertion sort

--- sorting.c
+++ sorting.c
@@ -7,3 +7,16 @@ void swap(int *x, int *y)
     *x = *y; 
     *y = temp; 
 }
+void insertionSort(int arr[], int n) 
+{ 
+    int i, key, j; 
+    for (i = 1; i < n; i++) { 
+        key = arr[i]; 
+        j = i - 1; 
+        while (j >= 0 && arr[j] > key) { 
+            arr[j + 1] = arr[j]; 
+            j = j - 1; 
+        } 
+        arr[j + 1] = key; 
+    } 
+} 
